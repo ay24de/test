fa1c80c3685cb9686e68b169dad6660d0316aaa5
ay24de <ay24de@gmail.com>
2024-03-24 18:02:44 +0100

    test commit

---
+++ 1.py
@@ -0,0 +1,18 @@
+print(1)
+print(1)
+print(1)
+print(1)
+print(3)
+
+def spaces():
+    return 1
+
+def tabs():
+        return 0
+    
+def main(var):
+#cursor is here->  Press tab
+    print(f'Ich bin {var}')
+# comment
+if __name__ == '__main__':
+    main()
